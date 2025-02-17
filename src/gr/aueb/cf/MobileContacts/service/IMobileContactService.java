f5e8bf371c6fa104a826b8169a664a7fad2d4d83
Philip Dimitriadis <philipdimitriadis@gmail.com>
2025-02-17 13:25:58 +0200

    Updates

--- src/gr/aueb/cf/MobileContacts/service/IMobileContactService.java
+++ src/gr/aueb/cf/MobileContacts/service/IMobileContactService.java
@@ -6,9 +6,14 @@ import gr.aueb.cf.MobileContacts.exceptions.ContactNotFoundException;
 import gr.aueb.cf.MobileContacts.exceptions.PhoneNumberAlreadyExistsException;
 import gr.aueb.cf.MobileContacts.model.MobileContact;
 
+import java.util.List;
+
 public interface IMobileContactService {
 
     MobileContact insertMobileContact(MobileContactInsertDTO dto) throws PhoneNumberAlreadyExistsException;
     MobileContact updateMobileContact(MobileContactUpdateDTO dto)
         throws PhoneNumberAlreadyExistsException, ContactNotFoundException;
+    void deleteContactById(Long id) throws ContactNotFoundException;
+    MobileContact getContactById(Long id) throws ContactNotFoundException;
+    List<MobileContact> getAllContacts();
 }
